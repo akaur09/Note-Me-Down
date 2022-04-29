e1cdbf8affa185d9dc6351f679b1477489b15f39
akaur091 <amniderkaur.09@gmail.com>
2022-04-28 21:52:56 -0700

    add const and app.use required

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const exp = require('constants');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
+
+
+
