1cba9641780e4c101f6ae18a5f82a54963451642
akaur091 <amniderkaur.09@gmail.com>
2022-04-28 22:04:07 -0700

    add app.post adn app.delete function

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
-const exp = require('constants');
 const express = require('express');
 const fs = require('fs');
+const { parse } = require('path');
 const path = require('path');
 
 const app = express();
@@ -11,5 +11,28 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(express.json());
 
+app.post('/api/notes', (req,res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+        if (err) throw err;
+        var notes =JSON.parse(data);
+        let userNote= req.body;
+        userNote.id = Math.floor(Math.random()* 5000);
+        notes.push(userNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes),(err, data) =>{
+            res.json(userNote);
+        });
+    });
+});
 
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+        if (err) throw err; 
+        let notes =JSON.parse(data);
+        const newNotes = notes.filter(note => note.is !== parseInt(req.params.id));
+        
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err,data) => {
+            res.json({msg:'sucess'});
+        });
+    });
+})
 
